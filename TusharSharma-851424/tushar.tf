a4a1d77d0f533324918df9c7b92b6ebabd011b98
iamtusharsharma <39986603+iamtusharsharma@users.noreply.github.com>
2020-02-18 16:06:50 +0530

    Update tushar.tf

--- TusharSharma-851424/tushar.tf
+++ TusharSharma-851424/tushar.tf
@@ -120,26 +120,7 @@ resource "aws_subnet" "main-public-1" {
         Name = "main-public-1"
     }
 }
-resource "aws_subnet" "main-public-2" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
 
-    tags {
-        Name = "main-public-2"
-    }
-}
-resource "aws_subnet" "main-public-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-public-3"
-    }
-}
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
@@ -148,25 +129,5 @@ resource "aws_subnet" "main-private-1" {
 
     tags {
         Name = "main-private-1"
-    }
-}
-resource "aws_subnet" "main-private-2" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.5.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
-
-    tags {
-        Name = "main-private-2"
-    }
-}
-resource "aws_subnet" "main-private-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.6.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-private-3"
     }
 }
