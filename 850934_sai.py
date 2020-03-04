7a842a6175fc745d5e8d5e112b4efb495e9ec358
bodla sai <30978046+bodlasai@users.noreply.github.com>
2020-03-04 17:32:36 +0530

    Add files via upload

---
+++ 850934_sai.py
@@ -0,0 +1,31 @@
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+table.meta.client.get_waiter('table_exists').wait(TableName='users')
+print(table.item_count)
