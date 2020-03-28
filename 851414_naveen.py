c46b75cfdc2426e607721d9c09ab57abd710279e
Naveen Shivnani <naveenshivnani586@gmail.com>
2020-03-28 18:26:51 +0530

    Add files via upload

---
+++ 851414_naveen.py
@@ -0,0 +1,12 @@
+from google.cloud import storage
+
+
+def create_bucket(bucket_name):
+    """Creates a new bucket."""
+    # bucket_name = "your-new-bucket-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.create_bucket(bucket_name)
+
+    print("Bucket {} created".format(bucket.name))
